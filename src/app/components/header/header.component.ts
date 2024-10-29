dbd9babf208979498b68502ecd2e134f533b01a2
Shreya <shreya.adh09@gmail.com>
2024-10-29 23:19:43 +0530

    Resolve merge conflicts and update services component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,37 +1,18 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgFor,NgIf],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'] 
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
-  dropdownOpen = false;
 
-  menuOptions = [
-    { label: 'Home' },
-    { label: 'Pages'},
-    { label: 'Insurances' },
-    { label: 'Blog'},
-    { label: 'Contact Us'},
-  ];
+  isMobileMenuOpen = false;
 
-  focusSearch() {
-    this.searchInput.nativeElement.classList.remove('hidden');
-    this.searchInput.nativeElement.focus();
-  }
-
-  toggleSearch(show: boolean) {
-    if (!show) {
-      this.searchInput.nativeElement.classList.add('hidden');
-    }
-  }
-
-  toggleDropdown() {
-    this.dropdownOpen = !this.dropdownOpen;
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
 }
